9f154bfd8c95cda4a6254e75ebc6c5c218c02519
Manasij Mukherjee <manasij7479@gmail.com>
2017-06-20 14:41:32 +0530

    Display columns in test script

--- utils/test-ref.sh
+++ utils/test-ref.sh
@@ -3,7 +3,11 @@
 make
 make -C ..
 
+#python generate-many.py 10 100 90 30
+
 (for file in tests/*; do ./po-test $file; done) > /tmp/test
 (for file in outs/*; do ../z3.out $file; done) > /tmp/out
+
+pr -m -t /tmp/test /tmp/out
 
 diff /tmp/test /tmp/out
