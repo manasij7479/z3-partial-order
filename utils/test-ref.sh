65c9c76764f13b1c65cc6dbf0870357ba8878c12
Manasij Mukherjee <manasij7479@gmail.com>
2017-06-20 14:46:58 +0530

    Auto generate test cases if not already present

--- utils/test-ref.sh
+++ utils/test-ref.sh
@@ -3,7 +3,9 @@
 make
 make -C ..
 
-#python generate-many.py 10 100 90 30
+if [ ! -d "tests" ]; then
+  python generate-many.py 10 100 90 30
+fi
 
 (for file in tests/*; do ./po-test $file; done) > /tmp/test
 (for file in outs/*; do ../z3.out $file; done) > /tmp/out
