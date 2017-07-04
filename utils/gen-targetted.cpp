2de347219f47eda59c4e74724539927ecb24b8ac
Ashutosh Gupta <agupta@tifr.res.in>
2017-07-04 19:33:56 +0530

    auto

--- utils/gen-targetted.cpp
+++ utils/gen-targetted.cpp
@@ -5,9 +5,19 @@
 #include <string>
 #include <fstream>
 #include <cassert>
+#include <vector>
+#include <tuple>
 
-bool do_int = true;
+bool do_int = false;
+int num_clauses = 300000;
+int clause_size = 3;
+int neg_prob = 50;
 
+inline std::string get_ord_name() {
+  if( do_int )
+    return "<";
+  return "partial-order";
+}
 
 class Writer {
 public:
@@ -22,18 +32,52 @@ public:
     if( do_int ) t_name = "Int";
     out << "(declare-fun v" << x << " () " << t_name << ")\n";
   }
+
   void writePositveAtom(int a, int b) {
-    std::string ord_name = "partial-order";
-    if( do_int ) ord_name = "<";
-    out << "(assert (" << ord_name << " v" << a << " v" << b << "))\n";
+    out << "(" << get_ord_name() << " v" << a << " v" << b << ")";
     test << a << ' ' << b << '\n';
   }
+
   void writeNegativeAtom(int a, int b) {
-    std::string ord_name = "partial-order";
-    if( do_int ) ord_name = "<";
-    out << "(assert (not (" << ord_name << " v" << a << " v" << b << ")))\n";
+    out << "(not ";
+    writePositveAtom(a,b);
+    out <<")";
     test << a << ' ' << b << '\n';
     // FIXME : Need a new syntax for test if positive and negative atoms can be mixed in the test.
+  }
+
+  void writeClause( std::vector< std::tuple<int,int,bool> > vec ) {
+    out << "(or ";
+    for( auto tup : vec ) {
+      int a = std::get<0>(tup);
+      int b = std::get<1>(tup);
+      int isPos = std::get<2>(tup);
+      if( isPos )
+        writePositveAtom(a,b);
+      else
+        writeNegativeAtom(a,b);
+      out << " ";
+    }
+    out <<")";
+  }
+
+  void writeAssertClause( std::vector< std::tuple<int,int,bool> > vec ) {
+    out << "(assert ";
+    writeClause(vec);
+    out <<")\n";
+  }
+
+  void writeAssertPositveAtom(int a, int b) {
+    out << "(assert ";
+    writePositveAtom(a,b);
+    out <<")\n";
+  }
+
+
+  void writeAssertNegativeAtom(int a, int b) {
+    out << "(assert ";
+    writeNegativeAtom(a,b);
+    out <<")\n";
   }
   ~Writer() {
     out << "(assert (distinct";
@@ -81,7 +125,7 @@ int main(int argc, char **argv) {
 
   	if (a != b && used.find(std::make_pair(a, b)) == used.end()) {
       if (b < a) std::swap(a, b);
-      writer.writePositveAtom(a, b);
+      writer.writeAssertPositveAtom(a, b);
       used.insert(std::make_pair(a, b));
   	} else {
       --i;
@@ -93,11 +137,30 @@ int main(int argc, char **argv) {
   	int b = std::rand() % num_vars;
   	if (a != b && used.find(std::make_pair(a, b)) == used.end()) {
       if (b < a) std::swap(a, b);
-      writer.writeNegativeAtom(a, b);
+      writer.writeAssertNegativeAtom(a, b);
       used.insert(std::make_pair(a, b));
   	} else {
       --i;
   	}
+  }
+
+
+  std::vector< std::tuple<int,int,bool> > vec;
+  int a,b;
+  bool isPos;
+
+  for(int i = 0; i < num_clauses; ++i ) {
+    for( int j =0; j < clause_size; j++ ) {
+      do{
+        a = std::rand() % num_vars;
+        b = std::rand() % num_vars;
+        is_pos = (std::rand() % 100) < neg_prob;
+      }while( a != b);
+      if (b < a) std::swap(a, b);
+      vec.push_back( std::make_tuple(a,b,is_pos) );
+    }
+    writer.writeAssertClause( vec );
+    vec.clear();
   }
   
   return 0;
