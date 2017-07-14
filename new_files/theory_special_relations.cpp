dee30f8aab41635336a9a17d00700394b5bf0f3c
Manasij Mukherjee <manasij7479@gmail.com>
2017-07-14 14:31:31 +0530

    Change to LRA

--- new_files/theory_special_relations.cpp
+++ new_files/theory_special_relations.cpp
@@ -80,8 +80,8 @@ namespace smt {
         params.set_bool("unsat_core", true);
 
 //        reg_decl_plugins(m_nested_ast_mgr);
-        m_nested_solver = mk_smt_solver(m, params, symbol("QF_LIA"));
-        m_int_sort = m_autil.mk_int();
+        m_nested_solver = mk_smt_solver(m, params, symbol("QF_LRA"));
+        m_int_sort = m_autil.mk_real();
     }
 
     theory_special_relations::~theory_special_relations() {
